fe188fb9195cbf3cc7e85066390313de33736c57
Steve Shreeve <steve.shreeve@gmail.com>
2023-02-03 16:20:41 -0700

    Remove PR reference

--- lib/censive.rb
+++ lib/censive.rb
@@ -9,7 +9,6 @@
 # • https://crystal-lang.org/api/1.7.2/CSV.html (Crystal's CSV library)
 # • https://github.com/ruby/strscan/blob/master/ext/strscan/strscan.c
 # • https://github.com/ruby/strscan/issues/50 for details
-# • https://github.com/ruby/strscan/pull/52 for code
 # ============================================================================
 # GOALS:
 #
