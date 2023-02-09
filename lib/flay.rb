0cdcedddcc3af0c178b6296010ad5bbb919d6ece
Steve Shreeve <steve.shreeve@gmail.com>
2023-02-09 16:47:16 -0700

    Start flay.rb - a quick benchmarker

---
+++ lib/flay.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+# ============================================================================
+# flay - A quick and lightweight benchmarking tool for Ruby
+#
+# Author: Steve Shreeve (steve.shreeve@gmail.com)
+#   Date: Feb 9, 2023
+# ============================================================================
+# GOALS:
+# 1. Provide a simple way to benchmark different code
+# 2. Easy to configure and quickly compare code
+#
+# TODO:
+# 1. Everything
+# ============================================================================
+
+
+
+config = {
+  name: "My Cool Benchmark",
+
+  contexts: [
+  ],
+  tasks: [
+
+  ],
+}
