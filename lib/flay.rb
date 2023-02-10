30140637d72770171165839a9a6344ba0cf1778b
Steve Shreeve <steve.shreeve@gmail.com>
2023-02-10 00:13:26 -0700

    Major progress to generating the generated scripts

--- lib/flay.rb
+++ lib/flay.rb
@@ -7,8 +7,8 @@
 #   Date: Feb 9, 2023
 # ============================================================================
 # GOALS:
-# 1. Provide a simple way to benchmark different code
-# 2. Easy to configure and quickly compare code
+# 1. Provide a simple way to benchmark various code
+# 2. Easy to configure and start comparing results
 #
 # TODO:
 # 1. Everything
@@ -38,19 +38,19 @@ config = {
   environments: [
     {
       name: "Environment 1",
-      before: <<~'|',
+      before: <<~"|",
         # Environment 1 before
       |
-      after: <<~'|',
+      after: <<~"|",
         # Environment 1 after
       |
     },
     {
       name: "Environment 2",
-      before: <<~'|',
+      before: <<~"|",
         # Environment 1 before
       |
-      after: <<~'|',
+      after: <<~"|",
         # Environment 1 after
       |
     },
@@ -59,23 +59,23 @@ config = {
   contexts: [
     {
       name: "Context 1",
-      before: <<~'|',
+      before: <<~"|",
         # context 1 before
       |
-      script: <<~'|',
+      script: <<~"|",
         a = [*1..1e5]
         a.sum
       |
-      after: <<~'|',
+      after: <<~"|",
         # context 1 after
       |
     },
     {
       name: "Context 2",
-      before: <<~'|',
+      before: <<~"|",
         # context 2 before
       |
-      after: <<~'|',
+      after: <<~"|",
         # context 2 after
       |
     },
@@ -84,19 +84,21 @@ config = {
   tasks: [
     {
       name: "Task 1",
-      before: <<~'|',
+      runs: 35,
+      before: <<~"|",
         # Task 1 before
       |
-      after: <<~'|',
+      after: <<~"|",
         # Task 1 after
       |
     },
     {
       name: "Task 2",
-      before: <<~'|',
+      secs: 30,
+      before: <<~"|",
         # Task 2 before
       |
-      after: <<~'|',
+      after: <<~"|",
         # Task 2 after
       |
     },
@@ -105,15 +107,114 @@ config = {
 
 # ==[ Helpers ]==
 
-def wrapper(object)
-  puts <<~"end"
-
-    # #{ "=" * 70 }
-    # #{object.name}
-    # #{ "=" * 70 }
-
-    #{object.before }
+def wrapper(object, type=nil)
+  case type
+  when :environment then puts template_for_environment object
+  when :context     then puts template_for_context     object
+  when :task        then puts template_for_task        object
+  else                   puts section                  object
   end
+end
+
+def wrap(list, type=nil, **opts)
+  list.each do |item|
+    wrapper(item, type)
+    yield item
+  end
+end
+
+def section(text, wide=78, left=0)
+  [
+    "# ".ljust(wide, "="),
+    "# #{text}",
+    "# ".ljust(wide, "="),
+  ].join("\n")
+end
+
+def hr(text, wide=78, left=0)
+  [ " " * left, "# ==[ ", text, " ]" ].join.ljust(wide, "=")
+end
+
+# ==[ Templates ]==
+
+def template_for_environment(environment)
+  <<~"|"
+    #{ section "Environment: #{environment.name} " }
+
+    # ==[ Code before environment ]==
+
+    #{ environment.before }
+  |
+end
+
+def template_for_context(context)
+  <<~"|"
+    #{ section "Context: #{context.name} " }
+
+    # ==[ Code before context ]==
+
+    #{ context.before }
+  |
+end
+
+def template_for_task(task)
+  <<~"|"
+    #{ section "Task: #{task.name} " }
+
+    # ==[ Code before task ]==
+
+    #{ task.before }
+
+    # ==[ Calculate the duration of a loop of empty runs ]==
+
+    if #{ task.runs } == 1
+      __flay_before_empty = 0
+      __flay_after_empty  = 0
+    else
+      __flay_before_empty = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      __flay_runs = 0
+
+      while __flay_runs < #{ task.runs }
+
+        # ==[ This empty loop improves the measurement for our real loop below ]==
+
+        __flay_runs += 1
+      end
+      __flay_after_empty = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    end
+
+    # ==[ Calculate the duration of a loop of script runs ]==
+
+    if #{ task.runs } == 1
+      __flay_before_script = 0
+      __flay_after_script  = 0
+    else
+      __flay_before_script = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      __flay_runs = 0
+      while __flay_runs < #{ task.runs }
+
+        # ==[ Before script ]==
+
+        #{ task.script }
+
+        # ==[ After script ]==
+
+        __flay_runs += 1
+      end
+      __flay_after_script = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    end
+
+    # ==[ Code after task ]==
+
+    #{ task.after }
+
+    # ==[ Write out timestamps ]==
+
+    __flay_duration = (__flay_after_script - __flay_before_script) -
+                      (__flay_after_empty  - __flay_before_empty )
+
+    File.write("/dev/null", __flay_duration.inspect)
+  |
 end
 
 # ==[ Workflow ]==
@@ -122,12 +223,9 @@ environments = config.environments
 contexts     = config.contexts
 tasks        = config.tasks
 
-environments.each do |environment|
-  wrapper(environment)
-  contexts.each do |context|
-    wrapper(context)
-    tasks.each do |task|
-      wrapper(task)
+wrap(environments, :environment) do |environment|
+  wrap(tasks, :task) do |task|
+    wrap(contexts, :context) do |context|
     end
   end
 end
