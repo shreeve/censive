a89c76e4f43e1ec6cf9f10280734e319b8d49a86
Steve Shreeve <steve.shreeve@gmail.com>
2023-02-09 22:23:00 -0700

    Implement a flexible Config hash/class

--- lib/flay.rb
+++ lib/flay.rb
@@ -14,10 +14,26 @@
 # 1. Everything
 # ============================================================================
 
+class Hash
+  def +@; Config[self]; end
+end
 
+class Config < Hash
+  def [](key, miss=nil)
+    key?(sym = key.to_sym) and return super(sym) || miss
+    ary = key.to_s.split(/(?:[.\/\[]|\][.\/]?)/)
+    val = ary.inject(self) do |obj, sub|
+      if    obj == self        then super(sub.to_sym)
+      elsif obj == nil         then break
+      elsif sub =~ /\A-?\d*\z/ then obj[sub.to_i]
+      else                          obj[sub.to_sym]
+      end
+    end or miss
+  end
 
 config = {
   name: "My Cool Benchmark",
+end
 
   contexts: [
   ],
