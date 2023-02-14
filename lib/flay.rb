a6d23f2b44af28460d7f93633f52c07d6929e3c9
Steve Shreeve <steve.shreeve@gmail.com>
2023-02-13 22:50:37 -0700

    First cleaned up version

--- lib/flay.rb
+++ lib/flay.rb
@@ -26,6 +26,7 @@ OptionParser.new.instance_eval do
 
 # on "-i"               , "--iterations", "Override the number of iterations", Integer
   on "-h"               , "--help"      , "Show help and command usage" do Kernel.abort to_s; end
+  on "-r"               , "--reverse"   , TrueClass
   on "-s <time,ips,spi>", "--stats "    , "Comma-separated list of stats (loops, time, ips, spi)"
   separator <<~"end"
 
@@ -41,9 +42,10 @@ OptionParser.new.instance_eval do
 end.parse!(into: opts={}) rescue abort($!.message)
 
 # runs = opts[:iterations]
-show = opts[:stats     ] || "time,ips,spi"
+show = opts[:stats] || "time,ips,spi"
 show = show.downcase.scan(/[a-z]+/i).uniq & %w[ ips loops spi time ]
 show.empty? and abort "invalid list of statistics #{opts[:stats].inspect}"
+swap = !opts[:reverse]
 
 class Hash
   alias_method :default_lookup, :[]
@@ -172,45 +174,55 @@ end
 
 # ==[ Workflow ]==
 
-flay = ARGV.first || "flay-2.rb"
-
+# read the flay script
+flay = ARGV.first or abort "missing flay script"
 code = ERB.new(DATA.read)
 
+# grok the config
 $config = eval(File.read(flay))
-
 es = $config.environments || [{}]
 cs = $config.contexts     || [{}]
 ts = $config.tasks        || [{}]
 
-# drawing
+# box drawing
 cols = stats(show)
 full = cols.map(&:size).sum + cols.size * 11 - 3
 wide = [*es.map {|e| e.name("").size}, *ts.map {|t| t.name("").size}].max
-@rt, @rm, @rb = boxlines(wide, cols.map {|e| e.size + 8 }, cs.size)
+rank = []
+
+# row: top, middle, bottom
+rt, rm, rb = boxlines(wide, cols.map {|e| e.size + 8 }, (swap ? cs : ts).size)
 
 # begin output
 puts "```"
 puts [$0, *ARGV].shelljoin
 puts IO.popen(["ruby", "-v"].join(" "), &:read)
 
-# let 'er rip!
+# loop over environment(s)
 es.each_with_index do |e, ei|
-  puts @rt
+  puts rt
 
   command = ["/Users/shreeve/.asdf/shims/ruby"]
-  @cb = "│ %-*.*s │" % [wide, wide, e.name(es.size > 1 ? "Env ##{ei + 1}" : "Task")]
-  @cb = cs.inject(@cb) {|s, c| s << " %-*.*s │" % [full, full, c.name("").center(full)] }
-  puts @cb
 
-  puts @rm
-  ts.each_with_index do |t, ti|
-    print "│ %-*.*s │" % [wide, wide, t.name]
-    cs.each_with_index do |c, ci|
+  # loop over context(s) and task(s)
+  ys, xs = swap ? [ts, cs] : [cs, ts]
+
+  # row: content, header
+  rc = "Task" # or "Context"
+  rh = "│ %-*.*s │" % [wide, wide, e.name(es.size > 1 ? "Env ##{ei + 1}" : rc)]
+  rh = xs.inject(rh) {|s, x| s << " %-*.*s │" % [full, full, x.name("").center(full)] }
+  puts rh, rm
+
+  ys.each_with_index do |y, yi|
+    print "│ %-*.*s │" % [wide, wide, y.name]
+    xs.each_with_index do |x, xi|
+    t, ti, c, ci = swap ? [y, yi, x, xi] : [x, xi, y, yi]
       delay = Tempfile.open(['flay-', '.rb']) do |file|
         loops = t.loops ||= 1e1 # || warmup(e, c, t)
         write(file, code.result(binding).rstrip + "\n") do |path|
           runs, time = execute(command, path)
           vals = stats(show, binding)
+          rank << [runs/time, ei, ci, ti]
           print vals.zip(cols).map {|pair| " %s │" % scale(*pair) }.join
         end
       end
@@ -218,7 +230,33 @@ es.each_with_index do |e, ei|
     print "\n"
   end
 end
-puts @rb
+puts rb
+
+# show the comparison
+rank.sort! {|a, b| b[0] <=> a[0] }
+fast = rank.first[0]
+slow = rank.last[0]
+pict = "%.2fx slower"
+last = (pict % slow).size
+cols = [11, last, 6]
+full = cols.sum + (cols.size - 1) * 3
+rt, rm, rb = boxlines(wide, cols)
+rh = "│ %-*.*s │" % [wide, wide, "Rank"]
+rh << " %-*.*s │" % [full, full, "Performance".center(full)]
+
+puts "", rt, rh, rm
+rank.each do |ips, ei, ci, ti|
+  y = swap ? cs[ci] : ts[ti]
+  print "│ %-*.*s │ %s │ " % [wide, wide, y.name, scale(ips, "i/s")]
+  if ips == fast
+    print "fastest".center(last)
+  else
+    print  "%*.*s" % [last, last, pict % [fast/ips]]
+  end
+  print " │ %-6s │\n" % ([ei+1,ci+1,ti+1] * "/")
+end
+puts rb
+puts "```"
 
 __END__
 
