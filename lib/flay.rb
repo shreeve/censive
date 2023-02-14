3dd67753807d0de73ad670f6f326c43480ed4723
Steve Shreeve <steve.shreeve@gmail.com>
2023-02-13 19:06:17 -0700

    Fix a small bug in box drawing

--- lib/flay.rb
+++ lib/flay.rb
@@ -104,7 +104,7 @@ def boxlines(main, cols, runs=1)
   .map do |str|
     list = [main, *(cols * runs)]
     list.map.with_index do |col, i|
-      chr = str[i < 3 ? i : (i - 1) % cols.size == 0 ? 1 : 2]
+      chr = str[i < 2 ? i : (i - 1) % cols.size == 0 ? 1 : 2]
       chr + str[3] * (col + 2)
     end.join + str[-1]
   end
